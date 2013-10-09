98ad8b094951d4b5323830a25405ee191b7b0ab2
Tomislav Ostojich <ostojch2@da5id.acm.uiuc.edu>
2013-10-08 22:08:20 -0500

    Committer: Tomislav Ostojich <ostojch2@illinois.edu>
     On branch master
    
     Initial commit
    
     Changes to be committed:
    
    	new file:   LICENSE
    	new file:   doc/intro.md
    	new file:   project.clj
    	new file:   src/aclm/core.clj
    	new file:   test/aclm/core_test.clj
    
     Untracked files:
       (use "git add <file>..." to include in what will be committed)
    
    	.gitignore

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject aclm "0.1"
+  :description "Pixel art program for CS 196"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [seesaw "1.4.4-SNAPSHOT"]]
+  :main aclm.core)
